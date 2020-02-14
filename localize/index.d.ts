55b16670347eb6a25ce255b5f6a12e1bfc395af6
Butkov Viktor Nikolaevich <ext.vbutkov@tinkoff.ru>
2020-02-14 11:16:01 +0500

    Выпилил все локализации окромя en и ru

---
+++ localize/index.d.ts
@@ -0,0 +1,2 @@
+export function en(errors: any): void;
+export function ru(errors: any): void;
--- localize/index.js
+++ localize/index.js
@@ -2,22 +2,5 @@
 
 module.exports = {
     en: require('./en'),
-    ar: require('./ar'),
-    cz: require('./cz'),
-    de: require('./de'),
-    es: require('./es'),
-    fr: require('./fr'),
-    hu: require('./hu'),
-    it: require('./it'),
-    ja: require('./ja'),
-    nb: require('./nb'),
-    nl: require('./nl'),
-    pl: require('./pl'),
-    'pt-BR': require('./pt-BR'),
     ru: require('./ru'),
-    sk: require('./sk'),
-    sv: require('./sv'),
-    th: require('./th'),
-    zh: require('./zh'),
-    'zh-TW': require('./zh-TW'),
 };
